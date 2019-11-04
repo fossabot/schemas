c52f56f47d1e4f94558a57783a66c4800affda2a
Peter Merkert <peter.merkert@gmail.com>
2019-11-04 17:01:26 +0200

    fix: Made claim creation and update body simpler and put logic into API code

--- request/claim/index.js
+++ request/claim/index.js
@@ -5,81 +5,42 @@ const querystring = require('./querystring')
 
 module.exports.create = {
 	body: {
-		oneOf: [
-			{
-				type: 'object',
-				required: ['type', 'referenceBadge'],
-				additionalProperties: false,
-				properties: {
-					claimantId: {type: 'string', minLength: 1},
-					geojson: generator.geoJSON,
-					explanation: {type: 'string', minLength: 1},
-					referenceClaimId: {type: 'string', minLength: 1},
-					referenceClaimVersion: {type: 'number', minimum: 1, multipleOf: 1},
-					referenceBadge: {type: 'string', minLength: 1},
-					otherCompanyId: {type: 'string', minLength: 1},
-					statusComment: {type: 'string', minLength: 1},
-					type: {type: 'string', const: constants.CLAIM_TYPE_ENUM.BADGE},
-					files: picture.list
-				}
-			},
-			{
-				type: 'object',
-				required: ['type', 'referenceCertificateId', 'referenceCertificateVersion'],
-				additionalProperties: false,
-				properties: {
-					claimantId: {type: 'string', minLength: 1},
-					geojson: generator.geoJSON,
-					explanation: {type: 'string', minLength: 1},
-					referenceClaimId: {type: 'string', minLength: 1},
-					referenceClaimVersion: {type: 'number', minimum: 1, multipleOf: 1},
-					referenceCertificateId: {type: 'string', minLength: 1},
-					referenceCertificateVersion: {type: 'number', minimum: 1, multipleOf: 1},
-					otherCompanyId: {type: 'string', minLength: 1},
-					statusComment: {type: 'string', minLength: 1},
-					type: {type: 'string', const: constants.CLAIM_TYPE_ENUM.CERTIFICATE},
-					files: picture.list
-				}
-			}
-		]
+		type: 'object',
+		required: ['type'],
+		additionalProperties: false,
+		properties: {
+			claimantId: {type: 'string', minLength: 1},
+			geojson: generator.geoJSON,
+			explanation: {type: 'string', minLength: 1},
+			referenceClaimId: {type: 'string', minLength: 1},
+			referenceClaimVersion: {type: 'number', minimum: 1, multipleOf: 1},
+			referenceBadge: {type: 'string', minLength: 1},
+			referenceCertificateId: {type: 'string', minLength: 1},
+			referenceCertificateVersion: {type: 'number', minimum: 1, multipleOf: 1},
+			otherCompanyId: {type: 'string', minLength: 1},
+			statusComment: {type: 'string', minLength: 1},
+			type: {type: 'string', enum: constants.CLAIM_TYPE},
+			files: picture.list
+		}
 	}
 }
 
 module.exports.update = {
 	body: {
-		oneOf: [
-			{
-				type: 'object',
-				required: ['referenceBadge'],
-				additionalProperties: false,
-				properties: {
-					geojson: generator.geoJSON,
-					explanation: {type: 'string', minLength: 1},
-					referenceClaimId: {type: 'string', minLength: 1},
-					referenceClaimVersion: {type: 'number', minimum: 1, multipleOf: 1},
-					referenceBadge: {type: 'string', minLength: 1},
-					otherCompanyId: {type: 'string', minLength: 1},
-					statusComment: {type: 'string', minLength: 1},
-					files: picture.list
-				}
-			},
-			{
-				type: 'object',
-				required: ['referenceCertificateId', 'referenceCertificateVersion'],
-				additionalProperties: false,
-				properties: {
-					geojson: generator.geoJSON,
-					explanation: {type: 'string', minLength: 1},
-					referenceClaimId: {type: 'string', minLength: 1},
-					referenceClaimVersion: {type: 'number', minimum: 1, multipleOf: 1},
-					referenceCertificateId: {type: 'string', minLength: 1},
-					referenceCertificateVersion: {type: 'number', minimum: 1, multipleOf: 1},
-					otherCompanyId: {type: 'string', minLength: 1},
-					statusComment: {type: 'string', minLength: 1},
-					files: picture.list
-				}
-			}
-		]
+		type: 'object',
+		required: [],
+		additionalProperties: false,
+		properties: {
+			geojson: generator.geoJSON,
+			explanation: {type: 'string', minLength: 1},
+			referenceClaimId: {type: 'string', minLength: 1},
+			referenceClaimVersion: {type: 'number', minimum: 1, multipleOf: 1},
+			referenceBadge: {type: 'string', minLength: 1},
+			referenceCertificateId: {type: 'string', minLength: 1},
+			referenceCertificateVersion: {type: 'number', minimum: 1, multipleOf: 1},
+			statusComment: {type: 'string', minLength: 1},
+			files: picture.list
+		}
 	}
 }
 
--- test/request/claim/index.test.js
+++ test/request/claim/index.test.js
@@ -60,68 +60,15 @@ test('Valid extensive badge claim', t => {
 	)
 })
 
-test('Detect invalid claims (neither badge nor certificate)', t => {
-	t.false(
-		ajv.validate(schemas.request.claim.create.body, {
-			type: 'BADGE'
-		})
-	)
-
-	t.false(
-		ajv.validate(schemas.request.claim.create.body, {
-			type: 'CERTIFICATE'
-		})
-	)
-})
-
 test('Valid extensive certificate claim update', t => {
 	t.true(
 		ajv.validate(schemas.request.claim.update.body, {
+			explanation: 'e',
 			referenceCertificateId: 'certificate-id',
 			referenceCertificateVersion: 1,
-			otherCompanyId: 'company-id',
+			referenceBadge: 'badge-id',
 			statusComment: 'Would love to have it!',
 			files: [{id: 'file-id'}]
-		})
-	)
-})
-
-test('Cannot update claim with both badge and certificate ID', t => {
-	t.false(
-		ajv.validate(schemas.request.claim.update.body, {
-			referenceBadge: 'other-badge-id',
-			referenceCertificateId: 'certificate-id',
-			referenceCertificateVersion: 1
-		})
-	)
-})
-
-test('Update must be eithere having still reference badge or reference certificate', t => {
-	t.false(
-		ajv.validate(schemas.request.claim.update.body, {
-			referenceCertificateId: 'certificate-id'
-		})
-	)
-
-	t.false(
-		ajv.validate(schemas.request.claim.update.body, {
-			referenceCertificateVersion: 1
-		})
-	)
-
-	t.true(
-		ajv.validate(schemas.request.claim.update.body, {
-			referenceCertificateId: 'certificate-id',
-			referenceCertificateVersion: 1,
-			otherCompanyId: 'company-id',
-			statusComment: 'Would love to have it!',
-			files: [{id: 'file-id'}]
-		})
-	)
-
-	t.true(
-		ajv.validate(schemas.request.claim.update.body, {
-			referenceBadge: 'badge-id'
 		})
 	)
 })
